b3907560f8b78a010aaef419396b5d327029f687
lucas.gime <gimenez_lm92@hotmail.com>
2023-12-07 23:15:29 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import ItemListContainer from "./components/ItemListContainer"
+import Navbar from "./components/Navbar"
+
+const App = () => {
+  return (
+    <div>
+      
+      
+      <Navbar/>
+      <ItemListContainer greeting={"Bienvenidos a Dharma Ecommerce"}/>
+
+
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import CartWidget from './CartWidget'
+
+const Navbar = () => {
+  return (
+    <div>
+        
+        <p>Dharma Ecommerce</p>
+        <CartWidget/>
+        
+    </div>
+  )
+}
+
+export default Navbar
